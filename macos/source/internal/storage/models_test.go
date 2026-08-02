f14df96794833ed2f0dc8c55e181985dc88ec66e
wufeng <xia6887@gmail.com>
2026-08-03 02:08:31 +0800

    Fix Windows file permission test

--- macos/source/internal/storage/models_test.go
+++ macos/source/internal/storage/models_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 )
 
@@ -20,7 +21,7 @@ func TestModelStoreUsesPrivateAtomicFile(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if info.Mode().Perm() != 0o600 {
+	if runtime.GOOS != "windows" && info.Mode().Perm() != 0o600 {
 		t.Fatalf("model file permissions = %o, want 600", info.Mode().Perm())
 	}
 	loaded, err := LoadModels()
